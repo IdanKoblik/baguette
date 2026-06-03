49652a4b6ac4df386ca6f4fa26f00dbeff8af4f5
Idan Koblik <me@idank.dev>
2026-06-03 22:48:40 +0300

    feat: Add logging

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
 #include <wayland-client.h>
+#include "log.h"
 
 int main(void) {
     struct wl_display *display = wl_display_connect(NULL);
     if (!display) {
-        fprintf(stderr, "Error: Could not connect to a Wayland display.\n");
+        ERROR("Error: Could not connect to a Wayland display.");
         return 1;
     }
 
-    printf("Success: Connected to the Wayland display successfully!\n");
+    LOG("Success: Connected to the Wayland display successfully!");
+
     wl_display_disconnect(display);
     return 0;
 }
