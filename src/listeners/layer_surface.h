d9641fdb888951e137578f15165a66c0bdd632a7
Idan Koblik <me@idank.dev>
2026-06-08 09:47:02 +0300

    remove src folder

--- src/listeners/layer_surface.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "../wlr-layer-shell-unstable-v1-protocol.h"
-
-void layer_surface_handle(void *data, struct zwlr_layer_surface_v1 *zwlr_layer_surface_v1, uint32_t serial, uint32_t width, uint32_t height);
--- src/ui.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "hud.h"
-
-int init_surface(struct hud_state *state);
-
-int init_buffer(struct hud_state *state);
-
-void hud_draw(struct hud_state *state, const char *left, const char *center, const char *right);
